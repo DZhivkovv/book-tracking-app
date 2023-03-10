1f1a709e9ccbb0da8d28f5f897700cbd55d7adcd
DZhivkovv <dobromirzhivkov01@gmail.com>
2023-03-10 15:32:40 +0200

    Add all needed components for the home page in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import headingImage from './img/—Pngtree—adult man reading a book_4430409.png'
 function App() {
   return (
     <div className="App">
-    
+      <div className='text-container'>
+        <h1>Book Track</h1>
+        <p>Some subtext</p>
+      </div>
+      <div className='image-container'> 
+        <img src={headingImage} alt='Homepage image.'   className='headingImage'></img>
+      </div>      
+      <div className='button-container'>
+        <button>Get started</button>
+      </div>
     </div>
 
   );
