ee5ba27a943298bfc2ccce7a68bcc2c1e38877d5
DZhivkovv <dobromirzhivkov01@gmail.com>
2023-03-12 15:25:05 +0200

    Style the homepage for mobile and desktop devices

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-import headingImage from './img/—Pngtree—adult man reading a book_4430409.png'
+import headingImage from './img/—Pngtree—hand drawn girl reading book_4050570.png'
 function App() {
   return (
     <div className="App">
       <div className='text-container'>
         <h1>Book Track</h1>
-        <p>Some subtext</p>
+        <p>Lorem ipsum dolor sit amet</p>
       </div>
       <div className='image-container'> 
         <img src={headingImage} alt='Homepage image.'   className='headingImage'></img>
       </div>      
-      <div className='button-container'>
-        <button>Get started</button>
-      </div>
     </div>
 
   );
