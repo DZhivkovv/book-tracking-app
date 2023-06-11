acea9be705632a844d0f825294b31286d4f2ef46
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-11 17:20:52 +0300

    feat: Add route to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home.jsx';
 import './assets/styles/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+  return(
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}/>          
+          </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
