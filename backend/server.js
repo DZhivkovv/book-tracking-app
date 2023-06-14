765b8f834390507a9c47e51231fcff54df0c873c
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-14 14:25:34 +0300

    refactor: Refactor server setup and add authentication routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();// Load environment variables from .env file
+
+import authRouter from './routes/auth.routes.js';
 import { checkEnvVariable } from './utils/utils.js';
 import { dbConnect } from './core/db.js';
 
+dotenv.config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/auth',authRouter); // Use the authentication routes defined in auth.routes.js
+
 const port = process.env.PORT;// Get the port variable from the .env file
 checkEnvVariable('PORT');// Check if a port variable exists in the .env file   
 
@@ -15,13 +23,6 @@ app.listen(port, () => {
     console.error(`Server failed to start: ${error}`);
 });
 
-
 const db_url = process.env.DB_URL;// Get the database url variable from the .env file
 checkEnvVariable('DB_URL');
 dbConnect(db_url);// Connect to the database
-
-app.get('/', (request, response) => {
-    response.send("Connected!")
-});
-
-
