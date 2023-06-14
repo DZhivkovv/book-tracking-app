0d43030a5b36bbc632ed3e3c78bfa201e1fd1d32
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-14 13:49:52 +0300

    feat: Add route to signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx';
+import Signup from './pages/Signup/Signup.jsx';
 import './assets/styles/App.scss';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home/>}/>          
+            <Route path='/signup' element={<Signup/>}/>          
           </Routes>
         </BrowserRouter>
     </div>
