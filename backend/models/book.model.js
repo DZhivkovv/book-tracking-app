0bc903c941263debeb7bd283b13dd7efd921ac11
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-15 23:06:01 +0300

    feat: Add the user's username and ID in the book model

--- backend/models/book.model.js
+++ backend/models/book.model.js
@@ -1,22 +1,37 @@
 import mongoose from 'mongoose'
+import User from './user.model.js'
 
 const bookSchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, "Title is a required field"],
   },
+
   author: {
     type: String,
     required: [true, "Author is a required field"],
   },
+
   pages: {
     type: Number,
     required: [true, "Pages is a required field"],
   },
+
   status: {
     type: String,
     enum: ['Read', 'Reading', 'Wants to read'],
     required: [true, "Status is a required field"],
+  },
+
+  postedBy:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  
+  username: {
+    type: String,
+    required: true,
   },
 });
 
