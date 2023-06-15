b3ac2e94b515856daeb434a114a9a29a9d968af1
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-15 20:24:49 +0300

    chore: Change the InputField components's key prop for input type radio

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -3,9 +3,9 @@ import InputField from "./InputField";
 
 export default function Form({ template, onSubmit, children }) {
   //Generates input fields based on the template defined in the parent component
-  const inputFields = template.map(({ label, type, placeholder, name, value, onChange, minLength, required }) => (    
+  const inputFields = template.map(({ label, type, placeholder, name, value, onChange, minLength, min, required }) => (    
     <InputField
-      key={name}
+      key={type="radio"? placeholder : name}
       label={label}
       type={type}
       placeholder={placeholder}
@@ -14,6 +14,7 @@ export default function Form({ template, onSubmit, children }) {
       onChange={onChange}
       minLength = {minLength}
       required={required}
+      min={min}
     />
   ));
 
