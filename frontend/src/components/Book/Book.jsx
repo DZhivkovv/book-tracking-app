426b0ab389bf93c0d8dc0337d05b1ab77e219c95
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-16 17:03:39 +0300

    feat: Add Book component

---
+++ frontend/src/components/Book/Book.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../assets/styles/Book.scss'
+
+export default function Book({id, title,author,status, onDelete}){
+    return(
+        <div className="book-container">
+            <div className="book-text">
+                <p className="book-title">{title}</p>
+                <p className="book-author">{author}</p>
+                <p className="book-status">{status}</p>
+            </div>
+            <button className="remove-book" onClick={() => onDelete(id)}>x</button>
+        </div>
+    )
+}
--- frontend/src/pages/Library/Library.jsx
+++ frontend/src/pages/Library/Library.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useAuthentication } from '../../hooks/useAuthentication.js'
 import { useBooks } from "../../hooks/useBooks.js";
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import Book from "../../components/Book/Book.jsx";
 import Footer from '../../components/Footer/Footer.jsx'
 
 import '../../assets/styles/Library.scss'
@@ -25,11 +26,9 @@ export default function Library() {
         setIsBookDeleted(true);
     };
 
-export default function Library() {    
-    const { userData, isLoggedIn} = useAuthentication();
-    const {books,booksLoaded} = useBooks();
-    const booksArray = booksLoaded ? books.map(book => <p>{book.title}</p>) : [];
-
+    // Maps over the books array that should contain all books added by a concrete user. For every book in that array, a Book component is generated. That happens only if booksLoaded is true, meaning that the book retrieving process from the database is over.
+    const booksArray = booksLoaded ? books.map(book => <Book key={book._id} id={book._id} title={book.title} author ={book.author} pages = {book.pages} status = {book.status} onDelete = {onDeleteBook}/> ): [];
+    
     useEffect(() => {
     //If a book has been deleted, the fetchBooks function will execute and retrieve all books from the user. This will update the page and display all the books, except the deleted one.
     if (isBookDeleted) {
@@ -42,7 +41,7 @@ export default function Library() {
         <div className="library-container">
             {/* Renders the Navbar component based on the user's authentication status */}
             {isLoggedIn ? 
-            <Navbar  paths={['Library','Attributions',`Welcome, ${userData.username}`,'Logout']}/> 
+            <Navbar  paths={['Home','Attributions',`Welcome, ${userData.username}`,'Logout']}/> 
             : 
             <Navbar  paths={['Library','Attributions', 'Log in', 'Sign Up']}/>
            }
