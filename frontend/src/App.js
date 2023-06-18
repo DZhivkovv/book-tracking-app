f5f723fcf97aadae222b6085c10c534e375d9661
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-18 20:23:04 +0300

    feat: Add attributions page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Signup from './pages/Signup/Signup.jsx';
 import Login from './pages/Login/Login.jsx';
 import Library from './pages/Library/Library.jsx';
 import AddBook from './pages/AddBook/AddBook.jsx';
+import Attributions from './pages/Attributions/Attributions.jsx';
 import './assets/styles/App.scss';
 
 export default function App() {
@@ -18,6 +19,7 @@ export default function App() {
             <Route path='/login' element={<Login/>}/>          
             <Route path='/library' element={<Library/>}/>          
             <Route path='/add-book' element={<AddBook/>}/>          
+            <Route path='/attributions' element={<Attributions/>}/>          
           </Routes>
         </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Attributions/Attributions.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Navbar from "../../components/Navbar/Navbar"
+import Footer from "../../components/Footer/Footer"
+
+import '../../assets/styles/attributions.scss'
+
+export default function Attributions(){
+    return(
+        <div className="attributions-container">
+            <Navbar
+                paths={['Home', 'Library']} //Each element in this prop represents a link in the navbar for this particular page.
+            ></Navbar>
+            <h1>Attributions</h1>
+                <a href="https://pngtree.com/freepng/hand-drawn-girl-reading-book-elements_4050570.html?sol=downref&id=bef" target="_blank">   
+                Reading clipart PNG Designed By 千图网</a> <br/>
+
+                <a href="https://www.flaticon.com/free-icons/education" title="education icons" target="_blank">Education icons created by Freepik - Flaticon</a><br/>
+                
+                <p>
+                    Photo by <a href="https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Annie Spratt</a> on <a href="https://unsplash.com/photos/gl7joOaABlI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a>
+                </p>
+
+                <p>
+                    Photo by <a href="https://unsplash.com/@trnavskauni?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Trnava University</a> on <a href="https://unsplash.com/photos/BEEyeib-am8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a>
+                </p>
+
+                <div className="attribution-social-media-icons">
+                    <a href="https://www.flaticon.com/free-icons/facebook" title="facebook icons" target="_blank">Facebook icons created by Freepik - Flaticon</a>
+                    <a href="https://www.flaticon.com/free-icons/github" title="github icons" target="_blank">Github icons created by Dave Gandy - Flaticon</a>
+                    <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons" target="_blank">Linkedin icons created by riajulislam - Flaticon</a>            
+                </div>
+
+            <Footer/>
+        </div>
+    )
+}
