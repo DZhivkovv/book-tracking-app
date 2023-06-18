3303db6eafada0434de99e350828239ea60a9bf5
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-18 11:47:42 +0300

    refactor: Add specific className to radio-buttons group only

--- frontend/src/components/Form/InputField.jsx
+++ frontend/src/components/Form/InputField.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function InputField({ value, label, name, placeholder, type, minLength, onChange, required,min }){
     //This component represents a form input field.
     return(
-        <div className="form-group">
+        <div className = {type==='radio'? "form-radio-group" : "form-group"}>
             {label && <label htmlFor="input-field">{label}</label>}
             <input
             type={type}
